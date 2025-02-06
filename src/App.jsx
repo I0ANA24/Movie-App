05729f9e62fe648a813c15449f7aafebf0ec3849
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2025-02-06 10:40:38 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App = () => {
   return (
-    
+    <main>
+      <div className="pattern" />
+      
+      <div className="wrapper">
+        <header>
+          <img src="../public/hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
   )
 }
 
