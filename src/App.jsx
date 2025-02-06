c8ec7a471a9b9fd0dbd8d62077066c93e611eeee
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2025-02-06 12:50:33 +0200

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern" />
@@ -11,7 +14,8 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy Without the Hassle</h1>
         </header>
 
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   )
